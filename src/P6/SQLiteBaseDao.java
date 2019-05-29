c755db66af27514adacc5a59d05f221c574b0473
Kevin Spelt <kevinspelt@gmail.com>
2019-05-29 11:20:09 +0200

    P6 fix

--- src/P6/SQLiteBaseDao.java
+++ src/P6/SQLiteBaseDao.java
@@ -7,7 +7,7 @@ public class SQLiteBaseDao {
     public Connection getConnection() {
         Connection conn = null;
         try {
-            conn = DriverManager.getConnection("jdbc:sqlite:C:/sqlite/db/P6.db");
+            conn = DriverManager.getConnection("jdbc:sqlite:src/P6/db/P6.db");
             System.out.println("Connection has been made");
         } catch (Exception e) {
             e.printStackTrace();
